1a6069638a2b231aeddbd473be2f1d9113d84a40
Aditya Gundigara <adityagundigara07@gmail.com>
2024-10-12 14:51:17 +0530

    hello everyone

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -4,10 +4,7 @@ const Homepage = () => {
     return (
 
         <>
-            <div>homepage</div>
-
-            <h2>Hello this is home</h2>
-            <h1>testing</h1>
+            <p>hello everyone</p>
         </>
     )
 }
