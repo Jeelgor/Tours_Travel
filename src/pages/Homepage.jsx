91e5a1e18b8b7b43071184dfcb39fb72ec24275b
Aditya Gundigara <adityagundigara07@gmail.com>
2024-10-13 13:39:12 +0530

    update

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,16 +3,7 @@ import React from 'react'
 const Homepage = () => {
     return (
         <>
-
-            <div>homepage</div>
-
-            <h2>Hello this is home</h2>
-            <h1>testing</h1>
-
-            <h1>please take pull</h1>
-
-            <p>hello everyone</p>
-
+            <p>hello world</p>           
         </>
     )
 }
